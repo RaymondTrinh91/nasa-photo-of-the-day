ecfa064a18637202c03d06704d786d1ae83d8e06
Raymond Trinh <RaymondTrinh91@gmail.com>
2019-10-09 14:47:05 -0700

    img displaying

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
+import NasaApod from "./Components/image";
+//https://api.nasa.gov/planetary/apod
+
+
 
 function App() {
+
+const [nasaImg, setNasaImg] = useState([]);
+
+//API GRAB
+useEffect(() => {
+  axios
+    .get("https://api.nasa.gov/planetary/apod?api_key=MMMT5Bs2HxfMUCSbeL0vb6RwB7hxqVJlRAw5uYNh")
+    .then(response => {
+      console.log(response.data);
+      setNasaImg(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+}, []);//Closes useEffect
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div className = "container">
+     
+          <NasaApod 
+          title={nasaImg.title} 
+          description={nasaImg.explanation} 
+          hdurl={nasaImg.hdurl} 
+          date={nasaImg.date} 
+          />
+      </div>
     </div>
   );
 }
---
+++ src/Components/image.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const NasaApod = props => {
+    return(
+        <div className = "imageContain">
+            <h2>{props.title}</h2>
+            <p>{props.desciption}</p> 
+            <img alt="A random photo from Nasa!" src={props.hdurl}/>
+            <p>{props.date}</p>
+        </div>
+    )
+}
+
+export default NasaApod;
