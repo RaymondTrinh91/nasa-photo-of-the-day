f8e9e3c32f3c7d7e5cac5a27c30974e91878cb91
Raymond Trinh <RaymondTrinh91@gmail.com>
2019-10-10 23:48:46 -0700

    MVP project 2 complete

--- src/App.js
+++ src/App.js
@@ -9,39 +9,49 @@ import ImageDescription from "./Components/ImageDescription";
 function App() {
 
 let today = new Date();
-let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-
+const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+let [modal, setModal] = useState(false);
 const [nasaImg, setNasaImg] = useState([]);
-const [imgDate] = useState(date);
-console.log(imgDate);
-console.log(today.getDate()-1);
+const [imgDate, setDate] = useState(date);
+
 //API GRAB
 useEffect(() => {
   axios
-    .get(`https://api.nasa.gov/planetary/apod?api_key=MMMT5Bs2HxfMUCSbeL0vb6RwB7hxqVJlRAw5uYNh&date=${imgDate}`)
+    .get(`https://api.nasa.gov/planetary/apod?api_key=59J7kSEYqSBeUVM6FoH5zF9vLbSe6aHsWnQwqPzS&date=${imgDate}`)
     .then(response => {
       console.log(response.data);
       setNasaImg(response.data);
+
     })
     .catch(error => {
       console.log(error);
     })
-}, [imgDate]);//Closes useEffect
+}, [imgDate]);//Closes API useEffect
+
+
+
+  useEffect(()=>{
+    window.addEventListener("click", toggle)
+  })
+
+  const toggle = () => {
+    setModal(modal = !modal);
+  }
 
   return (
     <div className="App">
       <div className = "container">
         <h1 className = "title">Nasa's Picture of the Day!</h1> 
-          <NasaApod 
-          title={nasaImg.title} 
-          description={nasaImg.explanation}
-          date={nasaImg.date}
+          <NasaApod  
           hdurl={nasaImg.hdurl}  
           />
           <ImageDescription
           title={nasaImg.title} 
           description={nasaImg.explanation}
           date={nasaImg.date}
+          toggle={toggle}
+          modal={modal}
+          setDate={setDate}
           />
       </div>
     </div>
